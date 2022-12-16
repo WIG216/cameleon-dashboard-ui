e76fcb684025feb33c7de761ee2df19cf06e7e3e
WIG216 <numbucliff04@gmail.com>
2022-12-16 23:19:22 +0100

    [Feature]: updates

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,19 @@
-sidebarToggler = document.getElementsByClassName(".sidebar-toggler");
+sidebarToggler = document.querySelector(".sidebar-ham");
+sidebarContainer = document.querySelector(".sidebar-container");
+contentWrapper = document.querySelector(".content-wrapper");
+title = document.querySelector(".title");
 
-sidebarToggler.addEventListener("click", ()=> {
-    alert("clicked");
-})
+sidebarToggler.addEventListener("click", showSidebar);
+
+function showSidebar() {
+  if (sidebarContainer.style.display === "block") {
+    sidebarContainer.style.display = "none";
+    contentWrapper.style.display = "block";
+    title.style.display = "flex";
+    title.style.flexWrap = "reverse";
+
+  } else {
+    sidebarContainer.style.display = " block";
+    // contentWrapper.style.display = "inline";
+  }
+}
